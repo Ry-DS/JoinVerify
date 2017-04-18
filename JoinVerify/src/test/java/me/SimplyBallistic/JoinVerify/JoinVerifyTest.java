4f2e3e28dd4367a3cb77c4dc56c81df2db26e26f
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-04-18 17:22:18 +1000

    Set up maven
    TODO fix gitignore

---
+++ JoinVerify/src/test/java/me/SimplyBallistic/JoinVerify/JoinVerifyTest.java
@@ -0,0 +1,38 @@
+package me.SimplyBallistic.JoinVerify;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class JoinVerifyTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public JoinVerifyTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( JoinVerifyTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
