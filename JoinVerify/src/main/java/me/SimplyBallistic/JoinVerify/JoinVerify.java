cfeae4535f09fc8effd643586284c9e67063f8de
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-04-18 17:20:11 +1000

    Set up maven

---
+++ JoinVerify/src/main/java/me/SimplyBallistic/JoinVerify/JoinVerify.java
@@ -0,0 +1,15 @@
+package me.SimplyBallistic.JoinVerify;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class JoinVerify extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		getLogger().info("Hello people");
+	}
+    
+}
