bcdbcf2ff045fc1674ab667c6628eb7555ccae07
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-04-20 21:55:57 +1000

    Sending data to server thru bungee

--- src/main/java/me/SimplyBallistic/JoinVerifyBungee/Listeners.java
+++ src/main/java/me/SimplyBallistic/JoinVerifyBungee/Listeners.java
@@ -1,5 +1,8 @@
 package me.SimplyBallistic.JoinVerifyBungee;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import com.google.common.io.ByteArrayDataOutput;
@@ -43,13 +46,37 @@ public class Listeners implements Listener {
 	}
 	@EventHandler
 	public void onJoin(ServerConnectedEvent e){
-		if(!plugin.file.containsPlayer(e.getPlayer().getUniqueId())){
+		if(plugin.file.containsPlayer(e.getPlayer().getUniqueId())){
 			plugin.getProxy().getScheduler().schedule(plugin, ()->{
 				 ByteArrayDataOutput out = ByteStreams.newDataOutput();
 				  
 				  out.writeUTF("Forward");
 				  out.writeUTF(e.getServer().getInfo().getName());
 				  out.writeUTF("JoinVerify");
+				  
+				  ByteArrayOutputStream msgbytes = new ByteArrayOutputStream();
+				  DataOutputStream msgout = new DataOutputStream(msgbytes);
+				  try {
+					msgout.writeUTF(e.getPlayer().getUniqueId().toString()+":verified");
+				} catch (IOException er) {
+					plugin.getLogger().warning("Failed in sending packet to server! Player will be forced to verify. "+er.getMessage());
+				} 
+				
+
+				  
+				  out.write(msgbytes.toByteArray());
+				  
+				  
+				  e.getPlayer().sendData("JoinVerify", out.toByteArray());
+				  
+				  
+				  try {
+					msgout.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				  
 
 				
 			}, 1, TimeUnit.SECONDS);
